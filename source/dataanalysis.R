e00b7b9c6dc29776fe36fce0dedb99a7e22caaad
LuCh2017 <30931030+LuCh2017@users.noreply.github.com>
2017-10-19 13:14:47 -0500

    Update dataanalysis.R
    Add question #4 solution.  Histograms and Scatter plots

--- source/dataanalysis.R
+++ source/dataanalysis.R
@@ -4,13 +4,12 @@
 ## 4	Plot the GDP for all of the countries. Use ggplot2 to color your plot by Income Group.
 ## 5	Cut the GDP ranking into 5 separate quantile groups. Make a table versus Income.Group. How many countries are Lower middle income but among the 38 nations with highest GDP?
 
-# use only columns country.code, counry.name, rank, gdp and Income.Group
-merged.data.sub<-merged.data[,c("country.code","country.name","rank","gdp","Income.Group")]
-
 ## 1  Merge the data based on the country shortcode. See how many of the IDs match;
-merged.data <- merge.data.frame(x = gdp, y = education, all = FALSE, by = 'country.code' )  # Contains only countries that appeared in both datasets
 nrow(merged.data)
 #[1] 189
+
+# use only columns country.code, country.name, rank, gdp and Income.Group
+merged.data.sub<-merged.data[,c("country.code","country.name","rank","gdp","Income.Group")]
 
 ## 2  Sort the data frame in ascending order by GDP (so United States is last). What is the 13th country in the resulting data frame?
 # Sort the data by GDP
@@ -29,5 +28,30 @@ hiNonOECD
 #[1] 104349.8
 
 ## 4	Plot the GDP for all of the countries. Use ggplot2 to color your plot by Income Group.
-install.packages("ggplot2")
 
+#Install ggplot2 package
+#install.packages("ggplot2")
+library(ggplot2)
+
+#Histogram of GDP for all countries, colors grouped by Income.Group
+ggplot(merged.data.sub, aes(x=country.code, y=gdp)) + 
+  # fill depends on Income.Group
+  geom_bar(aes(fill=Income.Group),  
+           stat="identity"
+ #          colour="black",    # ? Black outline for all?
+  )+
+  # Added axises title and hide x-axis text
+  labs(x = "Country",y="GDP(million)",title="GDP by Income Group") +
+  theme(axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())
+
+#Scatterplot of GDP for all countries, colors grouped by Income.Group
+ggplot(merged.data.sub, aes(x=country.code, y=gdp)) + 
+  # ?Draw lines in same group. colour, group both depend on Income.Group
+  # geom_line(aes(colour=Income.Group, group=Income.Group)) + 
+  # Set points. colour depends on Income.Group
+  geom_point(aes(colour=Income.Group),size=3)+ 
+  # # Added axises title and hide x-axis text
+  labs(x = "Country",y="GDP(million)",title="GDP by Income Group") +
+  theme(axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())
